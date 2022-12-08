7fee59bc15175cbc796aadb937d9cfeba13c5c9e
Stephen Quan <squan@esri.com>
2022-12-08 03:27:57 +0000

    Implement ImageBuffer.url

--- ImageBuffer.cpp
+++ ImageBuffer.cpp
@@ -1,4 +1,8 @@
 #include <QBuffer>
+#include <QQmlEngine>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
 #include "ImageBuffer.h"
 
 ImageBuffer::ImageBuffer(QObject * parent) :
@@ -30,3 +34,96 @@ QUrl ImageBuffer::toDataURL(const QString& format, const QVariant& options) cons
     return QString("data:image/png;base64,") + data.toBase64();
 }
 
+void ImageBuffer::setUrl(const QUrl& url)
+{
+    if (!url.isValid())
+    {
+        emit error(QString("Invalid URL"));
+        return;
+    }
+
+    if (url.isLocalFile())
+    {
+        setFilePath(url.toLocalFile());
+        return;
+    }
+
+    _setUrl(url);
+    _setFilePath(QString());
+
+    QQmlEngine* engine = qmlEngine(this);
+    if (!engine)
+    {
+        emit error(QString("No engine"));
+        return;
+    }
+
+    QNetworkAccessManager* manager = engine->networkAccessManager();
+    if (!manager)
+    {
+        emit error(QString("No QNetworkAccessManager"));
+        return;
+    }
+
+    QNetworkRequest request(url);
+    QNetworkReply* reply = manager->get(request);
+    connect(reply, &QNetworkReply::finished, this, &ImageBuffer::onFinished);
+}
+
+void ImageBuffer::setFilePath(const QString& filePath)
+{
+    _setUrl(QUrl::fromLocalFile(filePath));
+    _setFilePath(filePath);
+
+    if (!m_Image.load(filePath))
+    {
+        emit error(QString("Cannot load from file"));
+        return;
+    }
+
+    emit imageChanged();
+    emit loaded();
+}
+
+void ImageBuffer::onFinished()
+{
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+    if (!reply)
+    {
+        return;
+    }
+
+    QByteArray buffer = reply->readAll();
+    if (!m_Image.loadFromData(buffer))
+    {
+        emit error(QString("Cannot load"));
+        return;
+    }
+
+    emit imageChanged();
+    emit loaded();
+}
+
+void ImageBuffer::_setUrl(const QUrl& url)
+{
+    if (m_Url == url)
+    {
+        return;
+    }
+
+    m_Url = url;
+
+    emit urlChanged();
+}
+
+void ImageBuffer::_setFilePath(const QString& filePath)
+{
+    if (m_FilePath == filePath)
+    {
+        return;
+    }
+
+    m_FilePath = filePath;
+
+    emit filePathChanged();
+}
--- ImageBuffer.h
+++ ImageBuffer.h
@@ -13,11 +13,19 @@ class ImageBuffer : public QObject
     Q_PROPERTY(QVariant image READ image WRITE setImage NOTIFY imageChanged)
     Q_PROPERTY(int width READ width NOTIFY imageChanged)
     Q_PROPERTY(int height READ height NOTIFY imageChanged)
+    Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
+    Q_PROPERTY(QString filePath READ filePath WRITE setFilePath NOTIFY filePathChanged)
 
 public:
     ImageBuffer(QObject* parent = nullptr);
 
     static ImageBuffer* newImageBuffer(const QVariant& image, QJSEngine::ObjectOwnership objectOwnership = QJSEngine::JavaScriptOwnership);
+
+    QUrl url() const { return m_Url; }
+    void setUrl(const QUrl& url);
+
+    QString filePath() const { return m_FilePath; }
+    void setFilePath(const QString& filePath);
 
     Q_INVOKABLE QUrl toDataURL(const QString& format = QString(), const QVariant& options = QVariant()) const;
 
@@ -28,10 +36,22 @@ public:
     void setImage(const QVariant& image);
 
 signals:
+    void filePathChanged();
     void imageChanged();
+    void urlChanged();
+    void loaded();
+    void error(const QString& errorString);
+
+protected slots:
+    void onFinished();
 
 protected:
     QImage m_Image;
+    QUrl m_Url;
+    QString m_FilePath;
+
+    void _setUrl(const QUrl& url);
+    void _setFilePath(const QString& filePath);
 
 };
 
